740e1b4ac79fdfa945f99f4d786126fcd13f7320
ytokmakoff <ytokmakoff@gmail.com>
2024-06-20 22:57:10 +0300

    feat: add recommendation films

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -94,7 +94,6 @@ public class UserController {
     public List<Long> findAllFilmLikes(@PathVariable("id") long userId) {
         return userService.findAllFilmLikes(userId);
     }
-
     @DeleteMapping("/users/{userId}")
     public void deleteUser(@PathVariable long userId) {
         log.info("==> DELETE /users/" + userId);
